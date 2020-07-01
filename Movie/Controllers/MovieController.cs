53c30e86b3648fb044b2d1e07f5e844047041dff
namchok <namchok@NT1504095ITS>
2020-07-01 16:44:32 +0700

    Update add view and controller

---
+++ Movie/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, string id)
+        {
+            return $"Hello, Your name's {name}, your id's {id}";
+        }
+    }
+}
